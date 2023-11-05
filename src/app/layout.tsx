e850236bd26fa4f6cd856b0bf75e4f6d18f93844
Anna <annasukharieva0@gmail.com>
2023-11-05 21:22:25 +0200

    Merge branch 'dev' into calcLogic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = {
   title: 'Your Business Adviser | Калькулятор для Вашого Успішного Бізнесу',
   description:
     'Отримайте професійні бізнес-консультації та скористайтеся унікальним калькулятором бізнесу для підвищення ефективності вашої компанії',
-  };
-  
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
