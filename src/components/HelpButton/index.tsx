51cfa57d3b904d4afa4764fd72260ccc76b04fd4
Anna <annasukharieva0@gmail.com>
2023-12-06 18:26:23 +0200

    fix: * small fix

--- src/components/HelpButton/index.tsx
+++ src/components/HelpButton/index.tsx
@@ -41,9 +41,9 @@ export function HelpButton() {
             height="315"
             src="https://www.youtube.com/embed/ULKdxszRIMw?si=-sgsCE4JEZGxNGbP"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
           <GreenCustomButton handleClick={(event) => handleClickExample(event)}>
             Завантажити тренувальні дані
